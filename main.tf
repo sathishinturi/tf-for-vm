267e01a54c6abcf30b01254fd39c7746be16f7e2
sathishinturi <31530503+sathishinturi@users.noreply.github.com>
2019-07-25 11:11:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ data "aws_subnet_ids" "private" {
 resource "aws_instance" "web-1" {
     ami = "${lookup(var.amis, var.aws_region)}"
     availability_zone = "us-east-1a"
-    instance_type = "t2.nano"
+    instance_type = "t2.micro"
     key_name = "${var.key_name}"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
